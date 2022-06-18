403cfa5afee6e7a19ba1743bd7d978c5b56458ac
javiermills <javier_bouncing@hotmail.com>
2022-06-17 23:57:22 -0500

    addRouting

---
+++ src/app/Negocio_Internacional/pages/negocio-internacional/negocio-internacional.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-negocio-internacional',
+  templateUrl: './negocio-internacional.component.html',
+  styleUrls: ['./negocio-internacional.component.scss']
+})
+export class NegocioInternacionalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EspecialistacomexModule } from './EspecialistaComex/especialistacomex.module';
+import { NegocioInternacionalComponent } from './Negocio_Internacional/pages/negocio-internacional/negocio-internacional.component';
+import { LoginComponent } from './shared/start/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LoginComponent, pathMatch:'full'
+
+  },
+  {
+    path: 'negocio', component: NegocioInternacionalComponent, pathMatch:'full'
+
+  },
+  {
+    path: '', component: EspecialistacomexModule, pathMatch:'full'
+
+  },
+  {
+    path: '', component: LoginComponent, pathMatch:'full'
+
+  },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
