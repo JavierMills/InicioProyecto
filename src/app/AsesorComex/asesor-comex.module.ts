1ae59a4081b2f50552173c6641992828aa4e64b5
javiermills <javier_bouncing@hotmail.com>
2022-06-21 12:26:15 -0500

    boostrap add

--- src/app/AsesorComex/asesor-comex.module.ts
+++ src/app/AsesorComex/asesor-comex.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentsComponent } from './components/components.component';
 import { AsesorcomexComponent } from './pages/asesorcomex/asesorcomex.component';
-import { FormComponent } from './form/form.component';
+import { FormComponent } from './components/form/form.component';
 import { FolioComponent } from './components/folio/folio.component';
 
 
@@ -20,7 +20,10 @@ import { FolioComponent } from './components/folio/folio.component';
   ],
   exports:[
     ComponentsComponent,
-    AsesorcomexComponent
+    AsesorcomexComponent,
+    FormComponent,
+    FolioComponent
+    
   ]
 })
 export class AsesorComexModule { }
