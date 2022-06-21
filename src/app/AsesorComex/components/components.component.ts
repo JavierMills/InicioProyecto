d6e812b6f69074f2b272e4c964c673d6043a20cf
javiermills <javier_bouncing@hotmail.com>
2022-06-21 13:41:58 -0500

    agregue hover y booton de cerrar sesion

--- src/app/AsesorComex/components/components.component.ts
+++ src/app/AsesorComex/components/components.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComponentsComponent implements OnInit {
 
+  public Automaticas : string = 'Solicitudes Automaticas'
+  public Selectivas : string = 'Solicitudes Selectivas'
+  public TMEC : string = 'Solicitud TMEC'
+  public Contrato_PYME : string = 'Solicitud Contrato Pyme'
+  public Comex_TMEC : string = 'Solicitudes Comex-TMEC'
+  public Turismo : string = 'Solicitudes Turismo'
+
   constructor() { }
 
   ngOnInit(): void {
