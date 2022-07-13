5e12216951a85dbd0712686e43ae5eae25443f6a
Vediviere1992 <marco.morales@014payment.com>
2022-07-13 11:16:28 -0500

    Correcion complilacion

--- src/app/Contraloria/contraloria.module.ts
+++ src/app/Contraloria/contraloria.module.ts
@@ -33,12 +33,8 @@ import { AsesorComexModule } from '../AsesorComex/asesor-comex.module';
     CommonModule,
     DataTablesModule,
     FormsModule,
-<<<<<<< HEAD
-    SharedModule
-=======
     SharedModule,
     AsesorComexModule,
->>>>>>> 2d8a91e15a00703c9caf163e1352059c1de70630
   ],
   exports:[
     ContraloriaComponent
